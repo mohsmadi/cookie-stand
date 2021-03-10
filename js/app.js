f00b06a59f68e2f05b7f49d42344acdbe1907bb7
mohsmadi <msmadi_7@yahoo.com>
2021-03-11 01:39:38 +0200

    lab07

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 "use strict"
  
 let time=["6am " ,'7am ','8am','9am','10am','11am','12pm ','1pm ','2pm ','3pm ','4pm ','5pm ','6pm ','7pm '];
-//seattle:
+
+
 let seattleBranch = {
     location: 'Seattle Branch',
     min: 23,
@@ -51,7 +52,7 @@ let seattleBranch = {
 
 
    let tokyoBranch = {
-    location: 'tokyo Branch',
+    location: 'Tokyo Branch',
     min: 3,
     max: 24,
     avgCookieSale: 1.2,
@@ -230,3 +231,102 @@ let seattleBranch = {
 }
    limaBranch.hourlyDailySales();
    limaBranch.render();
+
+// let time=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+let tableElement=document.getElementById('table');
+
+function patCookies(branchName, minClintPerHour, maxClintPerHour, avgCookieSale){
+this.branchName=branchName;
+this.minClintPerHour;
+this.maxClintPerHour=maxClintPerHour;
+this.avgCookieSale=avgCookieSale;
+this.clintHourly =[];
+this.coockiesHourly =[];
+this.totalCoockiesPerDay = 0;
+}
+
+patCookies.prototype.calcClintHourly = function(){
+for(let x = 0; x < time.length; x++) {
+    this.calcClintHourly.push(random(this.minClintPerHour,maxClintPerHour));
+}
+};
+
+patCookies.prototype.calccookiesHourly = function(){
+    this.calcClintHourly();
+    for( let x = 0; x < time.length; x++){
+        let oneHour = Math.ceil(this.clintHourly[x] * this.avgCookieSale);
+        this.coockiesHourly.push(oneHour);
+        this.totalCoockiesPerDay +=oneHour;
+    }
+};
+
+patCookies.prototype.render = function(){
+    this.calccookiesHourly();
+    let tableRow = document.createElement('tr');
+    let tableDataElement= document.createElement('td');
+    tableDataElement.textContent=this.branchName;
+    tableRow.appendChild(tableDataElement);
+    
+    for (let x = 0; x < time.length; x++){
+       tableDataElement = document.createElement('td');
+       tableDataElement.textContenta = this.coockiesHourly[x];
+       tableRow.appendChild(tableDataElement);
+    }
+    let tableHeader = document.createElement('th');
+    tableHeader.textContent = this.totalCoockiesPerDay;
+    tableRow.appendChild(tableHeader);
+    tableElement.appendChild(tableRow);
+};
+// let branchList =[seattle, tokyo, dubai, paris, lima];
+seattle = new patCookies('Seattle', 23 , 65 , 6.3);
+tokyo = new patCookies('Tokyo', 3 , 24 , 1.2);
+dubai = new patCookies('Dubai', 11 , 38 , 3.7);
+paris = new patCookies('Paris', 20 , 38 , 2.3);
+lima = new patCookies('Lima', 2 , 16 , 4.6);
+
+
+function headerLine(){
+    let tableRow = document.createElement('tr');
+    let tableHeader = document.createElement('th');
+    tableHeader.textContent('Branchs');
+    tableRow.appendChild(tableHeader);
+
+    for (let x = 0; x < time.length; x++ ){
+      tableHeader.document.createElement('th');
+      tableHeader.textContent=time[x];
+      tableRow.appendChild(tableHeader);
+    } 
+    tableHeader = document.createElement('th');
+    tableHeader.textContent ='Branchs Total';
+    tableRow.appendChild(tableHeader);
+    tableElement.appendChild(tableRow);
+}
+
+function footerLine (){
+    let tableRow = document.createElement('tr'); 
+    let tableHeader = document.createElement('th');
+    tableHeader.textContent('Total Coockies Over The Branches per Hour');
+    tableRow.appendChild(tableHeader);
+    let totalSummation = 0;
+    for (let x = 0; x < time.length; x++);{
+      let totalPerHour = 0;
+      for ( y =0; y < branchList.length; y++);{
+          totalPerHour += branchList[y].coockiesHourly[x];
+          totalSummation +=branchList[y].coockiesHourly[x];
+      }
+      tableHeader=document.createElement('th');
+      tableHeader.textContent = totalPerHour;
+      tableRow.appendChild(tableHeader);
+    }
+    tableHeader = document.createElement('th');
+    tableHeader.textContent = totalSummation;
+    tableRow.appendChild(tableHeader);
+    tableElement.appendChild(tableRow);
+}
+(function renderTable (){
+    headerLine();
+    for ( let x =0; x < branchList.length; x++){
+        branchList.render();
+    }
+footerLine();
+})();
